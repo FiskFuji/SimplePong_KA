a36b6db2b15786050ea20343783d07dc3d61474f
KirkWorl <kworley@csumb.edu>
2016-10-01 11:06:33 -0700

    Updated portions of code to finish player classes and finish ball class. Updated main loop to include checks for positions. Addition of Rect objects to check collisions. Future update: Fix ball going through paddle, tweak ball velocity vectors. Final touches. Ball 'respawn' and score mechanics.

--- simplePong.py
+++ simplePong.py
@@ -28,7 +28,9 @@
 #------Begin Imports-----------------------------------------------------------#
 import pygame as pg
 import sys
+import random
 
+random.seed(None)
 pg.init()
 CL = pg.time.Clock()
 screen = pg.display.set_mode((400, 440))
@@ -43,6 +45,7 @@ class player:
     hitbox = pg.Rect((0, 0), (0, 0))
     score = 0
     
+    
     def __init__(self, startx, starty, width, height, (r, g, b)):
         self.posx = startx
         self.posy = starty
@@ -51,11 +54,14 @@ class player:
         self.RGB = (r, g, b)
         pos = pg.draw.rect(screen, self.RGB, ((startx, starty), (width, height)))
         self.hitbox = pg.Rect((self.posx, self.posy), (self.width, self.height))
-
+    
     def updatePlayer(self):
-        pos = pg.draw.rect(screen, self.RGB, ((self.posx, self.posy), (self.width, self.height)))
         self.hitbox.left = self.posx
         self.hitbox.top = self.posy
+        self.hitbox.width = self.width
+        self.hitbox.height = self.height
+        pos = pg.draw.rect(screen, self.RGB, ((self.posx, self.posy), (self.width, self.height)))
+    
 
 class ball:
     posx = 0
@@ -64,6 +70,7 @@ class ball:
     height = 0
     RGB = (0, 0, 0)
     velocity = [0, 0]
+    sideToGo = 1
     hitbox = pg.Rect((0, 0), (0, 0))
     
     def __init__(self, startx, starty, width, height, (r, g, b)):
@@ -72,17 +79,32 @@ class ball:
         self.width = width
         self.height = height
         self.RGB = (r, g, b)
-        self.velocity = [0, 0]
+        self.velocity = [-3, random.random()]
         pos = pg.draw.rect(screen, self.RGB, ((self.posx, self.posy), (self.width, self.height)))
         self.hitbox = pg.Rect((self.posx, self.posy), (self.width, self.height))
 
+    def moveBall(self):
+        self.posx += self.velocity[0]
+        self.posy += self.velocity[1]
+        
+    def updateBall(self):
+        self.moveBall()
+        pos = pg.draw.rect(screen, self.RGB, ((self.posx, self.posy), (self.width, self.height)))
+        self.hitbox.left = self.posx
+        self.hitbox.top = self.posy
+        self.hitbox.width = self.width
+        self.hitbox.height = self.height
+        
+
+
 def drawBoard():
     screen.fill((0, 0, 0))
-    pg.draw.rect(screen, (128, 128, 128), ((198, 5), (4, 12)))
-    pg.draw.rect(screen, (128, 128, 128), ((198, 25), (4, 12)))
-    pg.draw.rect(screen, (128, 128, 128), ((198, 45), (4, 12)))
-    pg.draw.rect(screen, (128, 128, 128), ((198, 65), (4, 12)))
-    pg.draw.rect(screen, (128, 128, 128), ((198, 85), (4, 12)))
+    
+    pg.draw.rect(screen, (128, 128, 128), ((198, 5),   (4, 12)))
+    pg.draw.rect(screen, (128, 128, 128), ((198, 25),  (4, 12)))
+    pg.draw.rect(screen, (128, 128, 128), ((198, 45),  (4, 12)))
+    pg.draw.rect(screen, (128, 128, 128), ((198, 65),  (4, 12)))
+    pg.draw.rect(screen, (128, 128, 128), ((198, 85),  (4, 12)))
     pg.draw.rect(screen, (128, 128, 128), ((198, 105), (4, 12)))
     pg.draw.rect(screen, (128, 128, 128), ((198, 125), (4, 12)))
     pg.draw.rect(screen, (128, 128, 128), ((198, 145), (4, 12)))
@@ -98,14 +120,25 @@ def drawBoard():
     pg.draw.rect(screen, (128, 128, 128), ((198, 345), (4, 12)))
     pg.draw.rect(screen, (128, 128, 128), ((198, 365), (4, 12)))
     pg.draw.rect(screen, (128, 128, 128), ((198, 385), (4, 12)))
-    pg.draw.rect(screen, (0, 255, 255),   ((0, 401),  (400, 2)))
+    pg.draw.rect(screen, (0,   255, 255), ((0,   401), (400, 2)))
+    
     screen.blit(scorefont.render(str(p1.score), 1, (255, 255, 255)), (5, 410))
     screen.blit(scorefont.render(str(p2.score), 1, (255, 255, 255)), (378, 410))
-    screen.blit(scorefont.render("SCORE", 1, (255, 255, 255)), (161, 410))
+    screen.blit(scorefont.render("SCORE",       1, (255, 255, 255)), (161, 410))
+
+    pg.draw.circle(screen, (128, 128, 128), (200, 201), 25, 1)
     
 
-p1 = player(5, 5, 15, 100, (255, 255, 255))
-p2 = player(380, 5, 15, 100, (255, 255, 255))
+p1      = player(5,   150, 15, 100, (255, 255, 255))
+p2      = player(380, 150, 15, 100, (255, 255, 255))
+ball    = ball(193, 193, 14, 14, (0, 255, 255))
+
+topOfScreen     = pg.Rect((0, 0),   (400, 1))
+bottomOfScreen  = pg.Rect((0, 401), (400, 1))
+player1Goal     = pg.Rect((0, 0),   (17, 401))
+player2Goal     = pg.Rect((400, 0),   (-17, 401))
+
+
 pg.key.set_repeat(1, 1)
 keys = []
 
@@ -113,35 +146,54 @@ while True:
 
     CL.tick(60)
     drawBoard()
+    ball.updateBall()
     p1.updatePlayer()
     p2.updatePlayer()
+    print ball.velocity
+    pg.draw.rect(screen, (255, 0, 0), player1Goal, 1)
+    pg.draw.rect(screen, (255, 0, 0), player2Goal, 1)
     pg.display.update()
-    mpos = pg.mouse.get_pos()
     keys = pg.key.get_pressed()
 
-    if((keys[pg.K_UP]) and (p1.posy >= 1) and (not keys[pg.K_DOWN])):
+    if((keys[pg.K_w]) and (p1.posy >= 1) and (not keys[pg.K_s])):
             p1.posy -= 3
 
-    if((keys[pg.K_DOWN]) and (p1.posy+100 <= 399) and (not keys[pg.K_UP])):
+    if((keys[pg.K_s]) and (p1.posy+100 <= 399) and (not keys[pg.K_w])):
         p1.posy += 3
 
-    if((keys[pg.K_w]) and (p2.posy >= 1) and (not keys[pg.K_s])):
+    if((keys[pg.K_UP]) and (p2.posy >= 1) and (not keys[pg.K_DOWN])):
         p2.posy -= 3
 
-    if((keys[pg.K_s]) and (p2.posy+100 <= 399) and (not keys[pg.K_w])):
+    if((keys[pg.K_DOWN]) and (p2.posy+100 <= 399) and (not keys[pg.K_w])):
         p2.posy += 3
+
+    if( (ball.hitbox.colliderect(p1.hitbox)) and (not ball.hitbox.colliderect(player1Goal)) ):
+        ball.velocity[0] = -ball.velocity[0]
+        if(not ball.velocity[0] > 3.2):
+            ball.velocity[0] += 0.1
+            if((ball.velocity[1] > 0) and (not ball.velocity[1] < 1.7):
+               ball.velocity[1] += 0.05
+
+    if( (ball.hitbox.colliderect(p2.hitbox)) and (not ball.hitbox.colliderect(player2Goal)) ):
+       ball.velocity[0] = -ball.velocity[0]
+       if(not ball.velocity[0] < -3.2):
+           ball.velocity[0] -= 0.1
+
+    if( (ball.hitbox.colliderect(bottomOfScreen)) and (not ball.hitbox.colliderect(player1Goal)) ):
+        ball.velocity[1] = -ball.velocity[1]
+        if(not ball.velocity[1] < -1.7):
+            ball.velocity[1] -= 0.1
+        
+    if( (ball.hitbox.colliderect(topOfScreen)) and (not ball.hitbox.colliderect(player1Goal)) ):
+       ball.velocity[1] = -ball.velocity[1]
+       if(not ball.velocity[1] > 1.7):
+           ball.velocity[1] += 0.1
     
     for e in pg.event.get():
 
         if e.type == pg.QUIT:
             pg.quit(); sys.exit()
 
-        elif e.type == pg.MOUSEBUTTONDOWN and (p1.hitbox.collidepoint(mpos) == 1):
-            print "Inside rectangle!"
 
+    
 
-                
-
-            
-
-            
